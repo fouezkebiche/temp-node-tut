af8594a733c8db56d4e352833062c022fa87ad8a
kebiche fouez <kebichefouez@gmail.com>
2023-12-30 21:32:38 -0800

    First Commit

---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+const sayHi = require('./utiles')
+
+require('./mind-grenade')
+
+
+sayHi('fouez')
+sayHi(names.john)
+sayHi(names.peter)
---
+++ names.js
@@ -0,0 +1,11 @@
+// share
+const peter = "peter";
+const john = "john";
+// local
+const secret = "SUPER SECRET";
+
+module.exports = { john, peter };
+
+
+
+
